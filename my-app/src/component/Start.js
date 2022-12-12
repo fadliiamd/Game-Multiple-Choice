84ec052a36780b16fa16aaae6f6ae40124d8a86f
Fadli <fadliadadakholqi15@gmail.com>
2022-12-12 12:58:56 +0700

    Start reactjs

---
+++ my-app/src/component/Start.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Start = () => {
+    return (
+        <div className="card">
+            <div className="card-content">
+                <h1>Mulai Quiz</h1>
+                <p>Semoga Sukses</p>
+                <button className="button is-success is-medium">MULAI</button>
+            </div>
+        </div>
+    )
+}
+export default Start;
